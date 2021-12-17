5b413b803c6b18512c044146505225f8bb60d06e
Dhruv <samantdhruv@gmail.com>
2021-12-17 16:39:42 +0530

    solved the no image problem
    always check element section of site to check if the styling is working or not

--- src/components/HeroImage/HeroImage.style.js
+++ src/components/HeroImage/HeroImage.style.js
@@ -1,12 +1,12 @@
 import styled from "styled-components";
 
 export const Wrapper = styled.div`
-  background: linear-gradien(
+  background: linear-gradient(
       to bottom,
       rgba(0, 0, 0, 0) 41%,
       rgba(0, 0, 0, 0.65) 100%
     ),
-    url(${({ image }) => image}), var(--darkGrey);
+    url(${(props) => props.image}), var(--darkGrey);
   background-size: 100%, cover;
   background-position: center;
   height: 600px;
--- src/components/HeroImage/index.js
+++ src/components/HeroImage/index.js
@@ -6,9 +6,6 @@ import { Wrapper, Content, Text } from "./HeroImage.style";
 
 const HeroImage = ({ image, title, text }) => (
   <Wrapper image={image}>
-    {/* <div className="temp">
-      <img src={image}/>
-    </div> */}
     <Content>
       <Text>
         <h1>{title}</h1>
