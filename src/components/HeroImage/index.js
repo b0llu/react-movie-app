34714b81de9985ee5507a3510991984ef8934079
Dhruv <samantdhruv@gmail.com>
2021-12-17 12:41:23 +0530

    created style.css to solve img not showing problem

--- src/components/HeroImage/index.js
+++ src/components/HeroImage/index.js
@@ -2,9 +2,13 @@ import react from "react";
 
 //styles
 import { Wrapper, Content, Text } from "./HeroImage.style";
+// import './style.css'
 
 const HeroImage = ({ image, title, text }) => (
   <Wrapper image={image}>
+    {/* <div className="temp">
+      <img src={image}/>
+    </div> */}
     <Content>
       <Text>
         <h1>{title}</h1>
