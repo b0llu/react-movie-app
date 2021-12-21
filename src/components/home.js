bd349061acc39a3b57fa83c3f5af4a844eb3c77b
Dhruv <samantdhruv@gmail.com>
2021-12-21 11:31:50 +0530

    creating the button logic to show more movies

--- src/components/home.js
+++ src/components/home.js
@@ -18,9 +18,12 @@ import { useHomeFetch } from "../../src/hooks/useHomeFetch";
 import NoImage from "../images/no_image.jpg";
 
 const Home = () => {
-  const { state, loading, error, setSearchTerm, searchTerm } = useHomeFetch();
+  const { state, loading, error, setSearchTerm, searchTerm, setIsLoadingMore } =
+    useHomeFetch();
 
   console.log(state);
+
+  if (error) return <div>Something went wrong... </div>;
 
   return (
     <>
@@ -52,7 +55,7 @@ const Home = () => {
       {loading && <Spinner />}
 
       {state.page < state.total_pages && !loading && (
-        <Button text="Load More" />
+        <Button text="Load More" callback={() => setIsLoadingMore(true)} />
       )}
     </>
   );
