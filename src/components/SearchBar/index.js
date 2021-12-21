254ccc986fca998912dc8d8ee8a8635d35a476ff
Dhruv <samantdhruv@gmail.com>
2021-12-21 10:32:44 +0530

    creating state for searchbar

--- src/components/SearchBar/index.js
+++ src/components/SearchBar/index.js
@@ -5,11 +5,18 @@ import seachIcon from "../../images/search-icon.svg";
 import { Wrapper, Content } from "./SearchBar.styles";
 
 const SearchBar = ({ setSearchTerm }) => {
+  const [state, setState] = useState("");
+
   return (
     <Wrapper>
       <Content>
         <img src={seachIcon} alt="search-icon" />
-        <input type="text" placeholder="Search Movie" />
+        <input
+          type="text"
+          placeholder="Search Movie"
+          onChange={(e) => setState(e.currentTarget.value)}
+          value={state}
+        />
       </Content>
     </Wrapper>
   );
