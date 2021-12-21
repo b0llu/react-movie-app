a710789184105a95ed02c627fb4623b590993f10
Dhruv <samantdhruv@gmail.com>
2021-12-21 10:56:46 +0530

    logic to take actual movie which is searched

--- src/hooks/useHomeFetch.js
+++ src/hooks/useHomeFetch.js
@@ -33,9 +33,11 @@ export const useHomeFetch = () => {
     setLoading(false);
   };
 
+  // initial and search
   useEffect(() => {
-    fetchMovies(1);
-  }, []);
+    setState(initialState);
+    fetchMovies(1, searchTerm);
+  }, [searchTerm]);
 
   return { state, loading, error, setSearchTerm };
 };
