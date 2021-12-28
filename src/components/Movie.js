468f6b9f41f8dfc7b51b0012e3c1e647e4d172fc
Dhruv <samantdhruv@gmail.com>
2021-12-28 10:12:14 +0530

    destructuring useMovieFetch in Movie

--- src/components/Movie.js
+++ src/components/Movie.js
@@ -1,13 +1,20 @@
 import react from "react";
+import { useParams } from "react-router-dom";
 
 import { IMAGE_BASE_URL, POSTER_SIZE } from "../config";
 
 import Grid from "./Grid";
 import Spinner from "./Spinner";
 
+import { useMovieFetch } from "../hooks/useMovieFetch";
+
 import NoImage from "../images/no_image.jpg";
 
 const Movie = () => {
+  const { movieId } = useParams();
+
+  const { state: movie, loading, error } = useMovieFetch(movieId);
+  console.log(movie);
   return (
     <>
       <div>Movie</div>
