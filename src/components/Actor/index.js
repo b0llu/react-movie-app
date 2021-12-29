974d68ab39839f98ee9a4e9bbba3246590602d71
Dhruv <samantdhruv@gmail.com>
2021-12-29 11:47:41 +0530

    showing the actors

--- src/components/Actor/index.js
+++ src/components/Actor/index.js
@@ -1,3 +1,13 @@
 import react from "react";
 
 import { Wrapper, Image } from "./Actor.style";
+
+const Actor = ({ name, character, imageUrl }) => (
+  <Wrapper>
+    <Image src={imageUrl} alt="actor-thumb" />
+    <h3>{name}</h3>
+    <p>{character}</p>
+  </Wrapper>
+);
+
+export default Actor;
